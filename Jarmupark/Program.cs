abeadae373fc595f490817b62ae368e85ab20bf5
Ripperoni <lcsabi9d@gmail.com>
2020-09-21 13:04:43 +0200

    Működő autó osztály

--- Jarmupark/Program.cs
+++ Jarmupark/Program.cs
@@ -10,6 +10,24 @@ namespace Jarmupark
     {
         static void Main(string[] args)
         {
+            auto lada = new auto("Piros","Lada2112",12);
+            auto trabant = new auto("zöld","Trabant 1.1",15);
+
+            lada.Tankol(20);
+            trabant.Tankol(20);
+           
+            lada.Allapot();
+            trabant.Allapot();
+            Console.WriteLine("Mennyit megy a lada: ");
+            int km = int.Parse(Console.ReadLine());
+            lada.Megy(km);
+            lada.Allapot();
+
+            Console.WriteLine("Mennyit megy a trabant: ");
+            int km2 = int.Parse(Console.ReadLine());
+            trabant.Megy(km2);
+            trabant.Allapot();
+            Console.ReadKey();
         }
     }
 }
---
+++ Jarmupark/auto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Jarmupark
+{
+    class auto
+    {
+        public string szin;
+        public string tipus;
+        public double uzemAnyag;
+        public double fogyaszt;
+        public double km;
+        public double megtehetokm;
+
+        public auto(string szin, string tipus, double fogyaszt)
+        {
+            this.szin = szin;
+            this.tipus = tipus;
+            this.fogyaszt = fogyaszt;
+            this.uzemAnyag = 0;
+            this.km = 0;
+            this.megtehetokm = 0;
+        }
+        public void Megteheto()
+        {
+            megtehetokm = uzemAnyag / fogyaszt * 100;
+        }
+        public void Tankol(double mennyit)
+        {
+            uzemAnyag += mennyit;
+            megtehetokm = uzemAnyag / fogyaszt * 100;
+        }
+        public void Megy(double km)
+        {
+            this.km += km;
+            uzemAnyag -= (km * fogyaszt / 100);
+            megtehetokm = uzemAnyag / fogyaszt * 100;
+        }
+        public void Allapot()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"{tipus} - {szin}");
+            Console.WriteLine($"Km óra: {km} km");
+            Console.WriteLine($"Üzemanyag: {uzemAnyag}1");
+            Console.WriteLine($"Megtehető km: {megtehetokm} km");
+            Console.WriteLine("-------------------------");
+            Console.ReadKey();
+        }
+    }
+}
+
